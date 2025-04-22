5b9d02e436d9c9f2d3623c7e58d9b98837415f40
Sergej Bakanow <81242217+SBakanow@users.noreply.github.com>
2025-04-22 23:58:13 +0200

    Add Constraints

--- src/main/java/de/seba/auth_service/model/User.java
+++ src/main/java/de/seba/auth_service/model/User.java
@@ -5,16 +5,31 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users", schema = "auth_service", uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @NotBlank
   private String username;
+
+  @NotBlank
   private String password;
+
+  public User() {
+
+  }
+
+  public User(String username, String password) {
+    this.username = username;
+    this.password = password;
+  }
 
   public Long getId() {
     return id;
