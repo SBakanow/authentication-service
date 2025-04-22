8ae5b61a9efbc6a50b97ef247521c3421fe38b5a
Sergej Bakanow <81242217+SBakanow@users.noreply.github.com>
2025-04-23 00:00:41 +0200

    Add custom Exceptions

---
+++ src/main/java/de/seba/auth_service/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package de.seba.auth_service.exception;
+
+public class UserAlreadyExistsException extends RuntimeException{
+
+  public UserAlreadyExistsException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/de/seba/auth_service/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package de.seba.auth_service.exception;
+
+public class UserNotFoundException extends RuntimeException{
+
+  public UserNotFoundException(String message) {
+    super(message);
+  }
+}
